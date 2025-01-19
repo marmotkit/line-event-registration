33da74b523bdb36755b0fa94de1cf866fb75ffc3
Golf Management System <golf_mgmt@example.com>
2025-01-19 22:59:53 +0800

    改善活動列表頁面設計

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,22 +1,36 @@
 import { useRouter } from 'next/router';
-import { Container, Button, Typography, Box } from '@mui/material';
+import { Container, Button, Typography, Box, AppBar, Toolbar } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
 
 export default function EventsPage() {
   const router = useRouter();
 
   return (
-    <Container maxWidth="lg">
-      <Box sx={{ mt: 4, mb: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          活動列表
-        </Typography>
-        <Button 
-          variant="contained" 
-          onClick={() => router.push('/events/new')}
-        >
-          建立新活動
-        </Button>
-      </Box>
-    </Container>
+    <>
+      <AppBar position="static" color="default" elevation={0}>
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            活動列表
+          </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            startIcon={<AddIcon />}
+            onClick={() => router.push('/events/new')}
+          >
+            建立新活動
+          </Button>
+        </Toolbar>
+      </AppBar>
+
+      <Container maxWidth="lg">
+        <Box sx={{ mt: 4 }}>
+          {/* 這裡是活動列表的內容 */}
+          <Typography variant="body1" color="text.secondary">
+            目前沒有活動
+          </Typography>
+        </Box>
+      </Container>
+    </>
   );
 }
